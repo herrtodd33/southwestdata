e9ea217f946bb55f187bcc0c677646a65551bb3f
Logan Schmidt <d4rkd0s@gmail.com>
2017-08-14 18:47:17 -0700

    Delete connect_to_db.php

--- connect_to_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DB login
-// host: 127.0.0.1
-// user: root
-// pass: ferrari308
-//  db: swd
-// Connect to database
-$database_connection = new mysqli("localhost", "root", "ferrari308", "swd");
