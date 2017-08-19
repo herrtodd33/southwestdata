7f7c33e1d0769eeebcc8eb6360e5012abe141788
Logan Schmidt <logo3801@gmail.com>
2017-08-18 17:48:58 -0700

    Fixed SQL

--- admin/add_plant.php
+++ admin/add_plant.php
@@ -1,8 +1,16 @@
 <?php
 // Include the database connection file
 include_once('../connect_to_db.php');
+// Get the new name
+$name = $_POST['name'];
+// Get the new description
+$description = $_POST['description'];
+// Get the new attributes
+$attributes = $_POST['attributes'];
+// Get the new name
+$photo =  $_POST['photo'];
 // The SQL for getting a list of plants from the database
-$add_plants_sql = "INSERT INTO plants id, name, description, attributes, photo VALUES(NULL, $_POST['name'], $_POST['description'], $_POST['attributes'], $_POST['photo'])";
+$add_plants_sql = "INSERT INTO plants ('id', 'name', 'description', 'attributes', 'photo') VALUES(NULL, $name, $description, $attributes, $photo)";
 // Run the SQL to Add the plant
 $mysqli_result_object = $database_connection->query($add_plants_sql);
 // Echo out the result
