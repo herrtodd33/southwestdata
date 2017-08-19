5df412af31b594638b756f16dd4ac692d4cdcd15
Logan Schmidt <logo3801@gmail.com>
2017-08-18 17:59:29 -0700

    Added more comments

--- admin/add_plant.php
+++ admin/add_plant.php
@@ -13,7 +13,9 @@ $photo =  $_POST['photo'];
 $add_plants_sql = "INSERT INTO plants (`name`, `description`, `attributes`, `photo`) VALUES('$name', '$description', '$attributes', '$photo')";
 // Run the SQL to Add the plant
 if ($database_connection->query($add_plants_sql) === TRUE) {
+    // Return success message
     echo "New plant added successfully";
 } else {
+    // Return error
     echo "Error: " . $add_plants_sql . "<br>" . $database_connection->error;
 }
