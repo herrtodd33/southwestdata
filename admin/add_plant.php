7cf929f354d15050f044a03d4afa4e6f1d43fa20
Logan Schmidt <logo3801@gmail.com>
2017-08-18 17:51:28 -0700

    Added debug

--- admin/add_plant.php
+++ admin/add_plant.php
@@ -11,7 +11,10 @@ $attributes = $_POST['attributes'];
 $photo =  $_POST['photo'];
 // The SQL for getting a list of plants from the database
 $add_plants_sql = "INSERT INTO plants ('id', 'name', 'description', 'attributes', 'photo') VALUES(NULL, $name, $description, $attributes, $photo)";
+// Echo the SQL
+echo "Running SQL: " . $add_plants_sql;
 // Run the SQL to Add the plant
 $mysqli_result_object = $database_connection->query($add_plants_sql);
+echo $mysqli_result_object;
 // Echo out the result
 print_r($mysqli_result_object);
