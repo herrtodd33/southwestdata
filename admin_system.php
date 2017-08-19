c0e765d744c560c4fa9c7e36c767fdf8460bace2
Logan Schmidt <logo3801@gmail.com>
2017-08-18 17:19:39 -0700

    Added admin system login

---
+++ admin_system.php
@@ -0,0 +1,13 @@
+<?php
+// Get the password function from the server
+include('/get_password');
+// Get the password from the function
+$password = getPassword();
+// Get the attempted login from the user
+$login = $_POST["password"];
+// Check if the attempt matches the server password
+if($login == $password) {
+    echo "Welcome!";
+} else {
+    echo "login failed!";
+}
