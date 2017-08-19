de109ee4a8e561cdf460f1371b377521b685b265
Logan Schmidt <logo3801@gmail.com>
2017-08-18 17:34:26 -0700

    Added plants and plant add form

--- admin_system.php
+++ admin_system.php
@@ -7,7 +7,62 @@ $password = getPassword();
 $login = $_POST["password"];
 // Check if the attempt matches the server password
 if($login == $password) {
-    echo "Welcome!";
+    // Successful login proceed with admin panel
+    ?>
+    <h2>Plants</h2>
+    <table border="1">
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Attributes</td>
+            <td>Picture</td>
+        </tr>
+        <tr>
+            <td>(autogen)</td>
+            <td><input type="text" name="name" placeholder="Name"></td>
+            <td><input type="text" name="description" placeholder="Description"></td>
+            <td><input type="text" name="attributes" placeholder="Attributes"></td>
+            <td><input type="text" name="picture" placeholder="Picture URL"></td>
+        </tr>
+        <?php
+        // Include the database connection file
+        include_once('connect_to_db.php');
+        // Set's up $database_connection object
+
+        // The SQL for getting a list of plants from the database
+        $get_plants_sql = "SELECT * FROM plants ORDER BY name ASC";
+
+        // Run the SQL
+        $mysqli_result_object = $database_connection->query($get_plants_sql);
+
+        // Get an associative array from the result object
+        $associative_array_of_plants = $mysqli_result_object->fetch_all(MYSQLI_ASSOC);
+
+        // Loop through each of the plants and make the links to the plant render page
+        foreach($associative_array_of_plants as $plant) {
+          echo "<tr>";
+          echo "<td>";
+          echo $plant["id"];
+          echo "</td>";
+          echo "<td>";
+          echo $plant["name"];
+          echo "</td>";
+          echo "<td>";
+          echo $plant["description"];
+          echo "</td>";
+          echo "<td>";
+          echo $plant["attributes"];
+          echo "</td>";
+          echo "<td>";
+          echo "<img style='width:30px; height:30px; border-radius:3px;' src='" . $plant['photo'] . "'><br><i>" . $plant['photo'] . "</i>";
+          echo "</td>";
+          echo "</tr>";
+        }
+        ?>
+    </table>
+    <?php
 } else {
-    echo "login failed!";
+    // Incorrect login display error
+    echo "Incorrect login...";
 }
