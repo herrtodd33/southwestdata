ec3d1a34198e4631f3c4a87d69a4882a5976810d
Todd Schmidt <herrtodd33@gmail.com>
2017-08-21 16:57:50 -0700

    added add_animal.php page
    added add_animal.php page

---
+++ admin/add_animal.php
@@ -0,0 +1,26 @@
+<?php
+// Include the database connection file
+include_once('../connect_to_db.php');
+// Get the new name
+$name = $_POST['name'];
+// Get the new description
+$description = $_POST['description'];
+// Get the new attributes
+$attributes = $_POST['attributes'];
+// Get the new name
+$photo =  $_POST['photo'];
+// Get the new lifespan
+$photo =  $_POST['lifespan'];
+// Get the new sound
+$photo =  $_POST['sound'];
+// The SQL for getting a list of animals from the database
+$add_animals_sql = "INSERT INTO animals (`name`, `description`, `attributes`, `photo`, `lifespan`, `sound`)
+VALUES('$name', '$description', '$attributes', '$photo', '$lifespan', '$sound' )";
+// Run the SQL to Add the animal
+if ($database_connection->query($add_animals_sql) === TRUE) {
+    // Return success message
+    echo "New animal added successfully";
+} else {
+    // Return error
+    echo "Error: " . $add_animals_sql . "<br>" . $database_connection->error;
+}
