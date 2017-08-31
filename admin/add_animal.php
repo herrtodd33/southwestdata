99ba05c4fa5dab36c954a39c8872f3179af8f905
Todd Schmidt <herrtodd33@gmail.com>
2017-08-30 20:24:03 -0700

    found missing space

--- admin/add_animal.php
+++ admin/add_animal.php
@@ -14,7 +14,7 @@ $lifespan =  $_POST['lifespan'];
 // Get the new sound
 $sound =  $_POST['sound'];
 // The SQL for getting a list of animals from the database
-$add_animals_sql = "INSERT INTO animals (`name`, `description`, `attributes`, `photo`, `lifespan`, `sound`) VALUES('$name', '$description', '$attributes', '$photo', '$lifespan', '$sound' )";
+$add_animals_sql = "INSERT INTO animals (`name`, `description`, `attributes`, `photo`, `lifespan`, `sound`) VALUES('$name', '$description', '$attributes', '$photo', '$lifespan', '$sound')";
 // Run the SQL to Add the animal
 if ($database_connection->query($add_animals_sql) === TRUE) {
     // Return success message
