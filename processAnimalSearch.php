70b4ddc13d4d31a444fd37d8b76a56e0df968d7d
Todd Schmidt <herrtodd33@gmail.com>
2017-09-10 15:42:46 -0700

    tweak

--- processAnimalSearch.php
+++ processAnimalSearch.php
@@ -22,7 +22,8 @@ if($mysqli_result_object->num_rows < 1) {
  die($mysqli_result_object->num_rows . " results were found matching <b>" . $searchterm . "</b>");
 }
 // Echo out a go back link
-echo "<a href='searchAnimal.php'>Go Back to All Mammals</a><br>";
+echo "<a href='searchAnimal.php'>Go Back to Search Mammals</a><br>";
+echo "<b>" "</b>"
 
 // Get an associative array from the result object
 $associative_array_of_animals = $mysqli_result_object->fetch_all(MYSQLI_ASSOC);
