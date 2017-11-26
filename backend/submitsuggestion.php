522ce3065d79cbd3ddc457e7bdd4c21ca2f7b387
Todd Schmidt <herrtodd33@gmail.com>
2017-11-26 16:12:25 -0700

    added suggestion box
    added suggestion box

--- backend/submitsuggestion.php
+++ backend/submitsuggestion.php
@@ -1,5 +1,16 @@
 <?php
+$suggestion = $_POST['suggestion'];
 
+// Include the database connection file
+include_once('connect_to_db.php');
+// Set's up $database_connection object
 
+// The SQL for getting a list of animals from the database
+$add_suggestion_query = "INSERT INTO suggestions (id, suggestion) VALUES NULL, " . $suggestion;
+
+// Run the SQL
+$database_connection->query($add_suggestion_query);
 
 ?>
+
+Submitted suggestion!
