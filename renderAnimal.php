c51f594b475508cec24b87bf256ddfac928673eb
Todd Schmidt <herrtodd33@gmail.com>
2017-11-26 15:43:12 -0700

    changed audio Dl option
    changed audio Dl option

--- renderAnimal.php
+++ renderAnimal.php
@@ -20,7 +20,6 @@ echo "<a href='index.php'>Go to Home Page</a><br><br>";
 // Echo out a go back link
 echo "<a href='animals.php'>Go Back to All Mammals</a><br>";
 
-
 // Check if we got any rows back
 if($mysqli_result_object->num_rows < 1) {
   // If we didn't then warn the user!
@@ -30,11 +29,20 @@ if($mysqli_result_object->num_rows < 1) {
 // Get an associative array from the result object
 $animal = $mysqli_result_object->fetch_assoc();
 
+// Code to remove the audio download button
+?>
+<style>
+video::-internal-media-controls-download-button { display:none; }
+video::-webkit-media-controls-enclosure { overflow:hidden; }
+video::-webkit-media-controls-panel { width: calc(100% + 30px); /* Adjust as needed */ }
+</style>
+<?php
+
 // Echo out the animal name as the page title
 echo "<h1>" . $animal['name'] . "</h1>";
 echo "<h3>Sound of the " . $animal['name'] . "</h3>";
 echo "<audio controls>";
-echo "<source src='" . $animal['sound'] . "' type='audio/mpeg'>";
+echo "<source src='" . $animal['sound'] . "' type='audio/mpeg' controlsList='nodownload'>";
 // echo "<p><a href='" . $animal['sound'] . "'>download sound</a></p>";
 echo "</audio><br>";
 echo "<h3>Lifespan: " . $animal['lifespan'] . " </h3>";
