4cad0e74ed5b717f9c40372e73285d48e391b2eb
Todd Schmidt <herrtodd33@gmail.com>
2017-11-26 16:17:48 -0700

    sugg update
    suggupatate

--- backend/submitsuggestion.php
+++ backend/submitsuggestion.php
@@ -2,11 +2,11 @@
 $suggestion = $_POST['suggestion'];
 
 // Include the database connection file
-include_once('connect_to_db.php');
+include_once('../connect_to_db.php');
 // Set's up $database_connection object
 
 // The SQL for getting a list of animals from the database
-$add_suggestion_query = "INSERT INTO suggestions (id, suggestion) VALUES NULL, " . $suggestion;
+$add_suggestion_query = "INSERT INTO suggestions (id, suggestion) VALUES (NULL, " . $suggestion . ")";
 
 // Run the SQL
 $database_connection->query($add_suggestion_query);
