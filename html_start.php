39e0edcaac5c8985dca008fa22c984e7f851e28f
Todd Schmidt <herrtodd33@gmail.com>
2017-11-26 17:49:53 -0700

    pic to 150 height

--- html_start.php
+++ html_start.php
@@ -4,7 +4,7 @@
   <title>Southwest Data</title>
 
   <table>
-  <tr><td><img src="/cowboyhome.jpg" width="200" height="200">
+  <tr><td><img src="/cowboyhome.jpg" width="200" height="150">
   </td></td>
   </table>
 
