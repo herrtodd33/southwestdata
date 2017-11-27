5928a553e01b674937dacd408e5b00f994025a3a
Todd Schmidt <herrtodd33@gmail.com>
2017-11-26 17:58:13 -0700

    font change

--- html_start.php
+++ html_start.php
@@ -12,7 +12,7 @@
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <style>
   p {
-    font-size: 18px;
+    font-size: 16px;
     font-family: sans-serif;
   }
   .swdphoto {
